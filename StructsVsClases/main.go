4962a2f99b7250a69b712d6c54dda617fed45a0f
Mario Espinoza <mario221649@gmail.com>
2024-03-11 11:34:46 -0700

    Metodos y Funciones

--- StructsVsClases/main.go
+++ StructsVsClases/main.go
@@ -13,4 +13,25 @@ func main() {
 	e.id = 1
 	e.name = "Name"
 	fmt.Println(e)
+	e.SetId(5)
+	e.SetName("Name 2")
+	fmt.Println(e)
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
+}
+
+func (e *Employee) SetId(id int) {
+	e.id = id
+}
+
+func (e *Employee) SetName(name string) {
+	e.name = name
+}
+
+func (e Employee) GetId() int {
+	return e.id
+}
+
+func (e Employee) GetName() string {
+	return e.name
 }

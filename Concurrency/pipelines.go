4cdd04a8a66b5175c92288e306bdad1e44e950c5
Mario Espinoza <mario221649@gmail.com>
2024-03-12 12:13:07 -0700

    Definiendo channels de lectura y escritura

---
+++ Concurrency/pipelines.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func Generator(c chan<- int) {
+	for value := 1; value <= 10; value++ {
+		c <- value
+	}
+	close(c)
+}
+
+func Double(in <-chan int, out chan<- int) {
+	for value := range in {
+		out <- 2 * value
+	}
+	close(out)
+}
+
+func Print(c <-chan int) {
+	for value := range c {
+		fmt.Println(value)
+	}
+}
+
+func main() {
+	generator := make(chan int)
+	doubles := make(chan int)
+
+	go Generator(generator)
+	go Double(generator, doubles)
+	Print(doubles)
+}

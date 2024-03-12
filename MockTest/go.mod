f52f8663b907756314e2654d1af4c0c1891c54a5
Mario Espinoza <mario221649@gmail.com>
2024-03-12 10:50:56 -0700

    Implementando Mocks

---
+++ MockTest/go.mod
@@ -0,0 +1,3 @@
+module github.com/Choclos15/testMocks
+
+go 1.22.1
--- MockTest/main_test.go
+++ MockTest/main_test.go
@@ -10,24 +10,55 @@ func TestGetFullTimeEmployeeById(t *testing.T) {
 		expectedEmployee FullTimeEmployee
 	}{
 		{
-			id: 1,
+			id:  1,
 			dni: "1",
 			mockFunc: func() {
-				GetEmoployeeById = func (id ind) (Employee, error) {
+				GetEmoployeeById = func(id int) (Employee, error) {
 					return Employee{
-						Id: 1,
+						Id:       1,
 						Position: "Programador",
 					}, nil
 				}
 
-				GetPersonByDNI = func (id string) (Person, error)  {
+				GetPersonByDNI = func(id string) (Person, error) {
 					return Person{
 						Name: "Mario Espinoza",
-						Age: 22,
-						DNI: "1",
+						Age:  22,
+						DNI:  "1",
 					}, nil
 				}
 			},
+			expectedEmployee: FullTimeEmployee{
+				Person: Person{
+					Age:  22,
+					DNI:  "1",
+					Name: "Mario Espinoza",
+				},
+				Employee: Employee{
+					Id:       1,
+					Position: "Programador",
+				},
+			},
+		},
+	}
+
+	originalGetEmployeeById := GetEmoployeeById
+	originalGetPersonByDNI := GetPersonByDNI
+
+	for _, test := range table {
+		test.mockFunc()
+		ft, err := GetFullTimeEmployeeById(test.id, test.dni)
+
+		if err != nil {
+
+			t.Errorf("Error When Getting Employee")
+		}
+
+		if ft.Age != test.expectedEmployee.Age {
+			t.Errorf("Error, Got %d Expected %d", ft.Age, test.expectedEmployee.Age)
 		}
 	}
+
+	GetEmoployeeById = originalGetEmployeeById
+	GetPersonByDNI = originalGetPersonByDNI
 }
